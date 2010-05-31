8c969474516d505881cea3196bd6c40623873663
Diego Muñoz Escalante <escalant3@gmail.com>
2010-05-31 20:54:13 +0200

    Refactores urls files

--- neo4j/urls.py
+++ neo4j/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
     (r'selector/$', 'plexigraph.neo4j.views.selector'),
-    (r'interactor_query', 'plexigraph.neo4j.views.interactor_query'),
     (r'new_query', 'plexigraph.neo4j.views.new_query'),
     (r'$', 'plexigraph.neo4j.views.index'),
 )
